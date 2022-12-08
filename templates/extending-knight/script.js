1e907fa5ad28adaa93c955ab435a70f7a82fea4c
Jeremy D Glebe <jeremyglebe@gmail.com>
2022-12-07 22:23:51 -0600

    toon shading

--- templates/extending-knight/script.js
+++ templates/extending-knight/script.js
@@ -58,10 +58,21 @@ uniform float glowPower;
 ${shader.fragmentShader}`;
                     // Replace the last line of the fragment shader with some new code
                     shader.fragmentShader = shader.fragmentShader.slice(0, -1) + `
-    vec4 glow = vec4( glowColor, 1.0 );
-    // vec4 outColor = diffuse + (glow * diffuse * glowPower);
-    // gl_FragColor = outColor;
-    gl_FragColor = gl_FragColor + (glow * gl_FragColor * glowPower);
+
+    // Now try to set all colors to some preset magnitudes
+    vec3 before = gl_FragColor.rgb;
+    float thresholds[4] = float[](0.0, 0.5, 1.0, sqrt(3.0));
+    // Get the magnitude of the color
+    float mag = length(before);
+    // Get the index of the threshold that is closest to the magnitude
+    int index = 0;
+    for (int i = 0; i < 4; i++) {
+        if (abs(thresholds[i] - mag) < abs(thresholds[index] - mag)) {
+            index = i;
+        }
+    }
+    // Scale the color so its magnitude is equal to the threshold
+    gl_FragColor.rgb = before * (thresholds[index] / mag);
 }`;
                     console.log(shader.fragmentShader);
                     shader.uniforms.glowColor = { value: new THREE.Color(0x00ff00) };
